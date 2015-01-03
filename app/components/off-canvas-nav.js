482492ba241ec2a3d530c70dd2d085007c6291dd
Morgan <rocketeer13@msn.com>
2015-01-02 23:52:00 -0800

    End of day 1, responsive off canvas nav sorta works!

--- app/components/off-canvas-nav.js
+++ app/components/off-canvas-nav.js
@@ -1,2 +1,28 @@
 import Ember from "ember";
-export default Ember.Component.extend({});
+var barsClass = "glyphicon glyphicon-align-justify offCanvasMoveButtonHidden";
+var arrowClass = "glyphicon glyphicon-chevron-left offCanvasMoveButtonVisible";
+
+export default Ember.Component.extend({
+    actions:{
+        toggleMenuVisible: function(){
+            this.set('visible', !this.get('visible'));
+            if(this.get('visible')){
+                $('.offCanvasNav').removeClass('slideout');
+                $('.offCanvasNav').addClass('slidein');
+            }
+            else{
+                 $('.offCanvasNav').removeClass('slidein');
+                $('.offCanvasNav').addClass('slideout');
+            }
+        }
+    },
+    visible: false,
+    iconClass : function(){
+        if(!this.get('visible')){
+            return barsClass;
+        }
+        else{
+            return arrowClass;
+        }
+    }.property('visible')
+});
